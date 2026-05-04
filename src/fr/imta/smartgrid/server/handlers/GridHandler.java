2aeda47e809e899d6d9bbd82a27ac07c2875f649
Paul ROSSIGNOL <paul.rossignol@imt-atlantique.net>
2026-05-04 08:18:12 +0200

    essaie 2

--- src/fr/imta/smartgrid/server/handlers/GridHandler.java
+++ src/fr/imta/smartgrid/server/handlers/GridHandler.java
@@ -2,6 +2,7 @@ package fr.imta.smartgrid.server.handlers;
 
 import java.util.List;
 
+import fr.imta.smartgrid.model.Grid;
 import io.vertx.ext.web.RoutingContext;
 import jakarta.persistence.EntityManager;
 
@@ -16,5 +17,13 @@ public class GridHandler {
         List<Integer> grids = db.createNativeQuery("SELECT g.id from grid as g").getResultList();
 
         ctx.json(grids);
+    }
+    public void getGrids(RoutingContext ctx){
+        grid = db.find(Grid.classInteger.parseInt(ctx.pathParam("id")));
+        if (g == null) {
+            ctx.fail(404);
+        } else {
+            ctx.json(g.toJSON());
+        }
     }
 }
